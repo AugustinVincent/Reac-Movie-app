ec0df59db9e2011b3e2a9b026add389d98265cd4
Arthur Thouin <arthurthouin@hotmail.com>
2021-03-25 19:53:03 +0100

    Router React

--- src/App.js
+++ src/App.js
@@ -31,108 +31,145 @@ document.addEventListener('scroll', (event) =>
   }
 })
 
-function App() {
-  const [movieDatas, setMovieDatas] = useState([])
-  const [firstMovie, setFirstMovie] = useState('empty')
-  const [searchField, setSearchField] = useState('')
-  const [pageNumber, setPageNumber] = useState(1)
-  let maxPageNumber = 5
-  useEffect(()=>
-  {
-    if(searchField !== '')
-    {
+function favoriteMovie() {
+  return <h2>Ur favorite movie is</h2>;
+  <FirstScreen></FirstScreen>
+}
 
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US&sort_by=popularity.desc&page=${pageNumber}&query=${searchField}`)
-      .then(result => result.json())
-      .then(result => 
-        {
-          fetch(`https://api.themoviedb.org/3/movie/${result.results?.[0]?.id}?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US`)
-          .then(res => res.json())
-          .then(res => {
-            setFirstMovie(res)
-          })
-          const tempArray = result.results
-          tempArray.shift()
-          setMovieDatas(tempArray)
-        })
-    }
-    else { 
-      console.log()
-      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0bb47688d9717ccbbc0f747be389c94a&sort_by=popularity.desc&page=${pageNumber}`)
-      .then(result => result.json())
-      .then(result => 
-        {
-          fetch(`https://api.themoviedb.org/3/movie/${result.results?.[0]?.id}?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US`)
-          .then(res => res.json())
-          .then(res => {
-            setFirstMovie(res)
-          })
-          const tempArray = result.results
-          tempArray.shift()
-          setMovieDatas(tempArray)
-        })
-      }
-  }, [searchField, pageNumber])
 
-  const upDateSearch = (e) =>
-  {
-    setSearchField(e.target.value)
-    setPageNumber(1)
-  }
-
-  // const updatePageNumber = () =>
-  // {
-  //   console.log(maxPageNumber)
-  //   if(pageNumber > 1) setPageNumber(pageNumber - 1)
-  //   else setPageNumber(pageNumber + 1)
-  // }
-
-  const previousPage = () =>
-  {
-    if(pageNumber > 1)
-    {
-
-      const tempNumber = pageNumber - 1
-      setPageNumber(tempNumber)
-
-      window.scrollTo({ top : 0, behavior : 'smooth'})
-    }
-  }
-
-  const nextPage = () =>
-  {
-    const tempNumber = pageNumber + 1
-    setPageNumber(tempNumber)
-
-    window.scrollTo({ top : 0, behavior : 'smooth'})
-  }
+function App(){
   return (
     <Router>
       <div>
         <div className="App">
-          <nav className="navbar header-navbar">
-                <div className="movie-time-logo">MOVIE TIME</div>
-                <div className="navbar-items-container">
-                    <div className="search-container">
-                        <input onChange={upDateSearch} type="text" className="search-field"/>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/VisualEditor_-_Icon_-_Search-big_-_white.svg/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png" alt="" className="search-icon"/>
-                    </div>
-                    <Link to= './'>Home</Link>
-                    <span><Link to= './fav'>Favorites</Link></span>
-                </div>
-            </nav>
+        <nav className="navbar header-navbar">
+              <div className="movie-time-logo"><Link to='/'>MOVIE TIME</Link></div>
+              <div className="navbar-items-container">
+                  <div className="search-container">
+                      <input type="text" className="search-field"/>
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/VisualEditor_-_Icon_-_Search-big_-_white.svg/1200px-VisualEditor_-_Icon_-_Search-big_-_white.svg.png" alt="" className="search-icon"/>
+                  </div>
+                  <span><Link to='/'>Home</Link></span>
+                  <span><Link to='/favorites'>Favorites</Link></span>
+              </div>
+          </nav>
         </div>
-          <FirstScreen firstMovie={firstMovie}/>
-            <FilmDisplay movieDatas = {movieDatas}/>
-            <div className="pages-btns">
-              <div onClick={previousPage} className="previous-btn btn">Previous</div>
-              <div onClick={nextPage} className="next-btn btn">Next</div>
-        </div>
+        <Switch>
+         <Route exact path="/">
+            <Home />
+         </Route>
+         <Route exact path="/favorites">
+            <Favorite />
+         </Route>
+      </Switch>
       </div>
     </Router>
-  )
-}
 
+  )}
+
+  function Home() {
+    const [movieDatas, setMovieDatas] = useState([])
+    const [firstMovie, setFirstMovie] = useState('empty')
+    const [searchField, setSearchField] = useState('')
+    const [pageNumber, setPageNumber] = useState(1)
+    let maxPageNumber = 5
+    useEffect(()=>
+    {
+      if(searchField !== '')
+      {
+  
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US&sort_by=popularity.desc&page=${pageNumber}&query=${searchField}`)
+        .then(result => result.json())
+        .then(result => 
+          {
+            fetch(`https://api.themoviedb.org/3/movie/${result.results?.[0]?.id}?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US`)
+            .then(res => res.json())
+            .then(res => {
+              setFirstMovie(res)
+            })
+            const tempArray = result.results
+            tempArray.shift()
+            setMovieDatas(tempArray)
+          })
+      }
+      else { 
+        console.log()
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=0bb47688d9717ccbbc0f747be389c94a&sort_by=popularity.desc&page=${pageNumber}`)
+        .then(result => result.json())
+        .then(result => 
+          {
+            fetch(`https://api.themoviedb.org/3/movie/${result.results?.[0]?.id}?api_key=0bb47688d9717ccbbc0f747be389c94a&language=en-US`)
+            .then(res => res.json())
+            .then(res => {
+              setFirstMovie(res)
+            })
+            const tempArray = result.results
+            tempArray.shift()
+            setMovieDatas(tempArray)
+          })
+        }
+    }, [searchField, pageNumber])
+  
+    const upDateSearch = (e) =>
+    {
+      setSearchField(e.target.value)
+      setPageNumber(1)
+    }
+  
+    // const updatePageNumber = () =>
+    // {
+    //   console.log(maxPageNumber)
+    //   if(pageNumber > 1) setPageNumber(pageNumber - 1)
+    //   else setPageNumber(pageNumber + 1)
+    // }
+  
+    const previousPage = () =>
+    {
+      if(pageNumber > 1)
+      {
+  
+        const tempNumber = pageNumber - 1
+        setPageNumber(tempNumber)
+  
+        window.scrollTo({ top : 0, behavior : 'smooth'})
+      }
+    }
+  
+    const nextPage = () =>
+    {
+      const tempNumber = pageNumber + 1
+      setPageNumber(tempNumber)
+  
+      window.scrollTo({ top : 0, behavior : 'smooth'})
+    }
+   
+    return (
+      <div className="App">
+        {/* <Navbar/> */}
+        <FirstScreen firstMovie={firstMovie}/>
+        <FilmDisplay movieDatas = {movieDatas}/>
+        <div className="pages-btns">
+          <div onClick={previousPage} className="previous-btn btn">Previous</div>
+          <div onClick={nextPage} className="next-btn btn">Next</div>
+        </div>
+  
+      </div>
+    );
+    
+    
+  
+  }
+
+function Favorite() {
+    return (
+        <div>Do not share screen</div>
+        )
+    }
+
+
+
+
+    
 export default App;
 
 
